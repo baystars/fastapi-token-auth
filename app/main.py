7fe0a8f973684edd675463d640276d57505219d4
Sankichi Okute <higebobo@gmail.com>
2020-11-18 13:49:37 +0900

    modified

--- app/main.py
+++ app/main.py
@@ -1,25 +1,8 @@
 # -*- mode: python -*- -*- coding: utf-8 -*-
-from fastapi import (Depends, FastAPI)
+from fastapi import FastAPI
 
-from fastapi.security import OAuth2PasswordRequestForm
-from .auth import (get_current_user, get_current_user_with_refresh_token,
-                   create_tokens, authenticate)
-from .schemas import (User, Token)
+from .endpoints import router
 
 app = FastAPI()
 
-@app.post("/token", response_model=Token)
-async def login(form: OAuth2PasswordRequestForm = Depends()):
-    """トークン発行"""
-    user = authenticate(form.username, form.password)
-    return create_tokens(user.id)
-
-@app.get("/refresh_token/", response_model=Token)
-async def refresh_token(current_user: User = Depends(get_current_user_with_refresh_token)):
-    """リフレッシュトークンでトークンを再取得"""
-    return create_tokens(current_user.id)
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    """ログイン中のユーザーを取得"""
-    return current_user
+app.include_router(router)
